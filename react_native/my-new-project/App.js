2b03311bc4705da8454a31e50344a289c71f512b
ellapresso <ellapresso@gmail.com>
2019-01-31 14:18:24 +0900

    weather app

--- react_native/my-new-project/App.js
+++ react_native/my-new-project/App.js
@@ -2,33 +2,52 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Weather from "./Weather";
 
+const API_KEY = "31a5f892f657e4842df666a9b39694d6";
+
 export default class App extends React.Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
-        console.log(error);
+        error: error;
       }
     );
   }
+
+  _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    )
+      .then(Response => Response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         <StatusBar barStyle="dark-content" />
         {isLoaded ? (
-          <Weather />
+          <Weather weatherName={name} temp={Math.floor(temperature - 273.15)} />
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingtext}>Getting Weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -40,6 +59,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
     flex: 1,
--- react_native/my-new-project/Weather.js
+++ react_native/my-new-project/Weather.js
@@ -2,23 +2,93 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
 import { Ionicons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component {
-  render() {
-    return (
-      <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
-        <View style={styles.upper}>
-          <Ionicons color="white" size={144} name="ios-rainy" />
-          <Text style={styles.temp}>35</Text>
-        </View>
-        <View style={styles.lower}>
-          <Text style={styles.title}>title</Text>
-          <Text style={styles.subtitle}>Rain Drop</Text>
-        </View>
-      </LinearGradient>
-    );
+// export default class Weather extends Component {
+//   render() {
+//     return (
+//       <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
+//         <View style={styles.upper}>
+//           <Ionicons color="white" size={144} name="ios-rainy" />
+//           <Text style={styles.temp}>35</Text>
+//         </View>
+//         <View style={styles.lower}>
+//           <Text style={styles.title}>title</Text>
+//           <Text style={styles.subtitle}>Rain Drop</Text>
+//         </View>
+//       </LinearGradient>
+//     );
+//   }
+// }
+
+const weatherCases = {
+  Rain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "비온댜",
+    subtitle: "우산챙기시오",
+    icon: "ios-rainy"
+  },
+  Clear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "맑음",
+    subtitle: "롱보드타쟈",
+    icon: "ios-sunny"
+  },
+  Thunderstorm: {
+    colors: ["#00ECBC", "#007AFD"],
+    title: "천둥번개",
+    subtitle: "우르르쾅쾅",
+    icon: "ios-thunderstorm"
+  },
+  Clouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "흐림",
+    subtitle: "우울우울열매",
+    icon: "ios-cloudy"
+  },
+  Snow: {
+    colors: ["#7DE2FC", "#B9B6E5"],
+    title: "눈내린다",
+    subtitle: "두유워너빌드어스노우맨",
+    icon: "ios-snow"
+  },
+  Drizzle: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "이슬비",
+    subtitle: "애매한우산",
+    icon: "ios-rain-outline"
   }
+};
+
+function Weather({ weatherName, temp }) {
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+    >
+      <View style={styles.upper}>
+        <Ionicons
+          color="white"
+          size={144}
+          name={weatherCases[weatherName].icon}
+        />
+        <Text style={styles.temp}>{temp}도</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired
+};
+export default Weather;
 
 const styles = StyleSheet.create({
   container: {
