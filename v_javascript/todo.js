0c193b565a15170de4d0317d274c4636c51c49f9
ellapresso <ellapresso@gmail.com>
2019-02-13 09:16:07 +0900

    list 등록, list보기

--- v_javascript/todo.js
+++ v_javascript/todo.js
@@ -3,16 +3,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const ToDos_Ls = "toDos";
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(ToDos_Ls, JSON.stringify(toDos)); //localstorage에 저장할 때, String으로 저장해주어야 함.
+}
 
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerHTML = "❌"; //charset utf-8 해줘야 이모지 나옴.
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+
+  delBtn.innerHTML = "❌"; //charset utf-8 해줘야 이모지 나옴.
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text, //text 만 써도 됨 (es6)
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +37,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(ToDos_Ls);
-  if (toDos != null) {
+  const loadedToDos = localStorage.getItem(ToDos_Ls);
+  if (loadedToDos != null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text); //to do list가 나타나게 함.
+    });
   }
 }
 
