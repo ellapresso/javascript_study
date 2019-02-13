7915e9e5c7e0a2d7bc25eb69ea16f61103827535
ellapresso <ellapresso@gmail.com>
2019-02-13 09:40:44 +0900

    delete to do list

--- v_javascript/todo.js
+++ v_javascript/todo.js
@@ -3,7 +3,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const ToDos_Ls = "toDos";
-const toDos = [];
+let toDos = [];
+
+function filterFn(roDo) {
+  return roDo.id === 1;
+}
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(ToDos_Ls, JSON.stringify(toDos)); //localstorage에 저장할 때, String으로 저장해주어야 함.
@@ -16,6 +31,7 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
 
   delBtn.innerHTML = "❌"; //charset utf-8 해줘야 이모지 나옴.
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
