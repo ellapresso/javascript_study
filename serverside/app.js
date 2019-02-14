4233ccf8ad05bf91616cb581d1047f927c365c0a
ellapresso <ellapresso@gmail.com>
2019-02-14 14:49:18 +0900

    express, jade(pug)

--- serverside/app.js
+++ serverside/app.js
@@ -3,11 +3,44 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public")); //정적인 파일이 위치할 디렉토리를 지정하는 부분
+app.locals.pretty = true;
 
+app.set("view engine", "jade"); //지금은 pug, 과거에는 jade
+app.set("views", "./views"); //templete file 저장할 폴더
+
+//정적인 파일이 위치할 디렉토리를 지정하는 부분 (Default : Public , 보통 html같은 파일을 위치함 )
+app.use(express.static("public"));
+
+app.get("/templete", function(req, res) {
+  res.render("temp", { time: Date(), _title: "pug" }); //templete.pug의 time변수에 time을 객체로 넘겨줌.
+});
 app.get("/", function(req, res) {
   res.send("Hello Home");
 }); //url을 치고 들어올때 get방식으로 받음.
+
+app.get("/dynamic", function(req, res) {
+  var lis = "";
+  for (let i = 0; i < 5; i++) {
+    lis = lis + "<li>coding</li>";
+  }
+  const time = Date();
+  const output = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>title</title>
+  </head>
+  <body>
+    This file name is 'static'
+    <ul>
+    ${lis}
+    </ul>
+    ${time}
+  </body>
+</html>
+`;
+  res.send(output);
+});
 
 app.get("/route", function(req, res) {
   res.send("Image is <img src='/3131.jpg'>");
