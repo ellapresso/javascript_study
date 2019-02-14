9d3efa7aada63d283b4ab60ce6820b36e95503cc
ellapresso <ellapresso@gmail.com>
2019-02-14 17:53:56 +0900

    get,post 설명, body-parser 설치

--- serverside/app.js
+++ serverside/app.js
@@ -1,9 +1,33 @@
 //main file, main application
 
 const express = require("express");
+const bodyParser = require("body-parser"); //미들웨어 : post방식으로 데이터를 보낼때 body객체를 읽어들임.
 const app = express();
 
 app.locals.pretty = true;
+
+app.set("view engine", "jade"); //지금은 pug, 과거에는 jade
+app.set("views", "./views"); //templete file 저장할 폴더
+
+//정적인 파일이 위치할 디렉토리를 지정하는 부분 (Default : Public , 보통 html같은 파일을 위치함 )
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/form", function(req, res) {
+  res.render("form");
+});
+
+app.post("/form_receiver", function(req, res) {
+  const title = req.body.title;
+  const description = req.body.description;
+  res.send(title + "," + description);
+});
+
+app.get("/form_receiver", function(req, res) {
+  const title = req.query.title;
+  const description = req.query.description;
+  res.send(title + "," + description);
+});
 
 app.get("/topic/:id", function(req, res) {
   const topics = ["Javascript ...", "Java ...", "PHP ...", "Nodejs ..."];
@@ -16,13 +40,6 @@ app.get("/topic/:id", function(req, res) {
   `; //쿼리스트링으로 하고싶을때는 query, 시멘틱으로 하고싶을때는 params
   res.send(output); //id가 쿼리스트링에서 넘겨주는 파라미터값
 });
-
-app.set("view engine", "jade"); //지금은 pug, 과거에는 jade
-app.set("views", "./views"); //templete file 저장할 폴더
-
-//정적인 파일이 위치할 디렉토리를 지정하는 부분 (Default : Public , 보통 html같은 파일을 위치함 )
-app.use(express.static("public"));
-
 app.get("/templete", function(req, res) {
   res.render("temp", { time: Date(), _title: "pug" }); //templete.pug의 time변수에 time을 객체로 넘겨줌.
 });
