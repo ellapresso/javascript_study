be46225e47dd0e0ce393bb304bd9a1e274709711
ellapresso <ellapresso@gmail.com>
2019-02-19 11:11:26 +0900

    로딩화면 후 리스트 출력

--- reactjs_movie_app/src/App.js
+++ reactjs_movie_app/src/App.js
@@ -9,44 +9,57 @@ import Movie from "./Movie";
 //   "https://pbs.twimg.com/profile_images/1082834871943258112/vf6z0ow6_400x400.jpg"
 // ];
 
-const movies = [
-  {
-    title: "HAPPY DEATH DAY",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Adventure_Time_cast.jpg/220px-Adventure_Time_cast.jpg"
-  },
-  {
-    title: "OLD BOY",
-    poster:
-      "https://img1.daumcdn.net/thumb/C216x312/?fname=http://i1.daumcdn.net/svc/image/U03/sdb/579174DA0263160002"
-  },
-  {
-    title: "STAR WORLDS",
-    poster:
-      "https://pbs.twimg.com/profile_images/1082834871943258112/vf6z0ow6_400x400.jpg"
-  }
-];
 class App extends Component {
   state = {
     greeting: "Hola!"
   };
+
   componentDidMount() {
     setTimeout(() => {
       // this.state.greeting = "somthing";
       this.setState({
-        greeting: "hello"
+        movies: [
+          {
+            title: "HAPPY DEATH DAY",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Adventure_Time_cast.jpg/220px-Adventure_Time_cast.jpg"
+          },
+          {
+            title: "OLD BOY",
+            poster:
+              "https://img1.daumcdn.net/thumb/C216x312/?fname=http://i1.daumcdn.net/svc/image/U03/sdb/579174DA0263160002"
+          },
+          {
+            title: "STAR WORLDS",
+            poster:
+              "https://pbs.twimg.com/profile_images/1082834871943258112/vf6z0ow6_400x400.jpg"
+          },
+          {
+            title: "frozen",
+            poster:
+              "https://img1.daumcdn.net/thumb/C216x312/?fname=http://i1.daumcdn.net/svc/image/U03/sdb/579174DA0263160002"
+          }
+        ]
+        // movies: [
+        //   ...this.state.movies //기존 영화 리스트
+        // ]
       });
     }, 2000); //2초뒤 바뀐 greeting값 출력
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movies, index) => {
+      return (
+        <Movie title={movies.title} poster={movies.poster} key={index} /> //컴포넌트가 많으면 유니크한 키값이 필요
+      );
+    });
+    return movies; //movies 에 데이터를 저장함.
+  };
+
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movies, index) => {
-          return (
-            <Movie title={movies.title} poster={movies.poster} key={index} /> //컴포넌트가 많으면 유니크한 키값이 필요
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
         {/* <Movie title={movieTitles[0]} poster={movieImages[0]} />
         <Movie title={movieTitles[1]} poster={movieImages[1]} />
         <Movie title={movieTitles[2]} poster={movieImages[2]} /> */}
