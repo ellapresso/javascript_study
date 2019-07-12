2c49992990cb6a6483629c359e3ecf965d96c071
ellapresso <ellapresso@gmail.com>
2019-07-12 14:13:21 +0900

    프로그래머스

--- programmers/12916.js
+++ programmers/12916.js
@@ -12,3 +12,24 @@ function solution(s) {
 }
 
 //이상한 문자 만들기
+function solution(s) {
+    var words = s.split(/ /, -1)
+    var whole = ''
+    words.map((e, i) => {
+        var char = e.split('')
+        var txt = ""
+        char.map((e, i) => {
+            e = ((i % 2 == 0) ? char[i] = e.toUpperCase() : e.toLowerCase())
+            txt = txt + e
+        })
+        whole = (whole == '') ? (txt) : (whole + " " + txt)
+    })
+    return (whole)
+}
+
+//멋쪙...
+function toWeirdCase(s) {
+    return s.toUpperCase().replace(/(\w)(\w)/g, function (a) {
+        return a[0].toUpperCase() + a[1].toLowerCase();
+    })
+}
